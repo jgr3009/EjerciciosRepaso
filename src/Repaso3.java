229a6b862e5247c0e2785ae37f3ca67c41967736
Jorge García Rojo <jgr3009@gmail.com>
2015-11-12 19:07:30 +0100

    Terminado el ejercicio de repaso3

---
+++ src/Repaso3.java
@@ -0,0 +1,51 @@
+
+/*
+ * Autor Jorge García Rojo
+ * 
+ * La clase Repaso 3 tiene que dibujar un rectángulo con la esquina superior izquierda
+ * en centro de la pantalla.
+ */
+
+
+
+import acm.graphics.*;
+
+
+public class Repaso3 extends acm.program.GraphicsProgram{
+
+	//una variable de instancia es lo que se declara antes del inicio para que esté vista en el código.
+	
+	//Declaro una variable de instancia para almacenar el rectángulo
+	//Como es de instancia, se ve desde el init y desde el run.
+	GRect rectangulo;
+	
+	//declaro otra variable de instancia que guardará el punto X de la pantalla
+	//en que empezará a pintarse el rectángulo
+	int distanciaX;
+	
+	//declaro otra variable de instancia que guardará el punto Y de la pantalla
+	//en que empezará a pintarse el rectángulo
+	int distanciaY;
+	
+	public void init(){
+		
+		//cambio el tamaño de la pantalla
+		setSize (800, 600);
+		
+		//creo el rectángulo de 120 de ancho por 80 de alto
+		rectangulo = new GRect (120, 80);
+		add(rectangulo);
+	}
+	
+	
+	public void run(){
+		
+		//divido el ancho de la pantalla en 2 para saber donde está la mitad exacta de la pantalla
+		
+		distanciaX = getWidth()/2;
+		
+		//divido el alto de la pantalla en 2 para saber donde está la mitad exacta de la pantalla
+		distanciaY = getHeight()/2;
+		rectangulo.setLocation(distanciaX, distanciaY);
+	}
+}
